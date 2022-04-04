c0c03e74284c0f249a6300a1ccfa5b4c7f5fd04e
Henrique Florencio <henrique.florencio307@gmail.com>
2022-04-04 17:42:37 -0300

    funções assincronas escritas de forma diferente...

--- index.js
+++ index.js
@@ -1,13 +1,45 @@
 //const chalk = require('chalk');
 import chalk from 'chalk';
+import fs from 'fs';
 
-const azul = chalk.blue
-const vermelho = chalk.red
+// DEVOLVE QUAL QUER QUE TENHA SIDO O ERRO CASO NÃO DOCUMENTADO NA LISTA
 
-const testoFuncao = `${azul("testo retornado")} por ${vermelho("uma função")}`
-
-function retornaTexto(texto){
-    return texto
+function trataErro(err){
+    switch(err.code){
+        case 'ENOENT':
+            throw new Error(chalk.yellow(err.code, "Caminho especificado ta errado"))
+        default:
+            throw new Error(chalk.yellow(err.code, "erro não especificado"))
+    }
 }
 
-console.log(retornaTexto(testoFuncao))
+async function pegaArquivo(caminho){
+    try{
+        const texto = await fs.promises.readFile(caminho, 'utf-8')
+        console.log(chalk.green(texto))
+    } catch(err) {
+        trataErro(err)
+    }
+}
+
+function pegaArquivo(caminho) {
+    fs.promises
+        .readFile(caminho, 'utf-8')
+        .then(texto => console.log(texto))
+        .catch(err => trataErro(err))
+}
+
+git 
+function pegaArquivo(caminho) {
+    fs.readFile(caminho, 'utf-8', (err, texto) => {
+        if(err) {
+            throw new Error(chalk.grey(err.code, "Caminho especificado ta errado"))
+         } else {
+            console.log(chalk.green(texto))
+         } 
+    })
+}
+
+pegaArquivo('./arquivos/text1.md')
+
+//console.log(chalk.green(pegaArquivo('./arquivos/texto1.md')))
