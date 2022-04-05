4cf4910c6552d19c84977d13f6b50234598e004e
henrique florencio <henrique.florencio307@gmail.com>
2022-04-05 11:24:04 -0300

    procurando links em um arquivo de texto com fs e regex!

--- index.js
+++ index.js
@@ -5,8 +5,6 @@ import fs from "fs";
 // REGEX https://regex101.com
 // DEVOLVE QUAL QUER QUE TENHA SIDO O ERRO CASO NÃO DOCUMENTADO NA LISTA
 
-const alo = "A interface File provê informações sobre arquivos e permite ao JavaScript  a acessar seu conteúdo.São geralmente recuperados a partir de um objeto [FileList](https://developer.mozilla.org/pt-BR/docs/Web/API/FileList) que é retornado como resultado da seleção, pelo usuário, de arquivos através do elemento [<input>](https://developer.mozilla.org/pt-BR/docs/Web/HTML/Element/Input), a partir do objeto [DataTransfer](https://developer.mozilla.org/pt-BR/docs/Web/API/DataTransfer) utilizado em operações de arrastar e soltar, ou a partir da API `mozGetAsFile()` em um [HTMLCanvasElement](https://developer.mozilla.org/pt-BR/docs/Web/API/HTMLCanvasElement). Em Gecko, códigos com privilégiios podem criar objetos File representando qualquer arquivo local sem a intereção do usuário (veja [Implementation notes](https://developer.mozilla.org/pt-BR/docs/Web/API/File#implementation_notes) para mais informações.)"
-
 function extraiLinks(texto){
     let regex = /\[([^\]]*)\]\((http?s:\/\/[^$#\s]*.[^\s]*)\)/gm;
     let arrayResultados = []
