1440e41e82302bfdf91a9b4dec445a5088086e0e
henrique florencio <henrique.florencio307@gmail.com>
2022-04-05 11:23:25 -0300

    procurando links em um arquivo de texto com fs e regex!

--- index.js
+++ index.js
@@ -2,7 +2,24 @@
 import chalk from "chalk";
 import fs from "fs";
 
+// REGEX https://regex101.com
 // DEVOLVE QUAL QUER QUE TENHA SIDO O ERRO CASO NÃO DOCUMENTADO NA LISTA
+
+const alo = "A interface File provê informações sobre arquivos e permite ao JavaScript  a acessar seu conteúdo.São geralmente recuperados a partir de um objeto [FileList](https://developer.mozilla.org/pt-BR/docs/Web/API/FileList) que é retornado como resultado da seleção, pelo usuário, de arquivos através do elemento [<input>](https://developer.mozilla.org/pt-BR/docs/Web/HTML/Element/Input), a partir do objeto [DataTransfer](https://developer.mozilla.org/pt-BR/docs/Web/API/DataTransfer) utilizado em operações de arrastar e soltar, ou a partir da API `mozGetAsFile()` em um [HTMLCanvasElement](https://developer.mozilla.org/pt-BR/docs/Web/API/HTMLCanvasElement). Em Gecko, códigos com privilégiios podem criar objetos File representando qualquer arquivo local sem a intereção do usuário (veja [Implementation notes](https://developer.mozilla.org/pt-BR/docs/Web/API/File#implementation_notes) para mais informações.)"
+
+function extraiLinks(texto){
+    let regex = /\[([^\]]*)\]\((http?s:\/\/[^$#\s]*.[^\s]*)\)/gm;
+    let arrayResultados = []
+    let temp;
+
+    while((temp = regex.exec(texto)) !== null){
+        arrayResultados.push({ [temp[1]]: temp[2] })
+    }
+
+    return arrayResultados
+}
+
+extraiLinks(alo)
 
 function trataErro(err) {
   switch (err.code) {
@@ -15,12 +32,26 @@ function trataErro(err) {
 
 async function pegaArquivo(caminho) {
   try {
-      const texto = await fs.promises.readFile(caminho, "utf-8");
-    console.log(chalk.green(texto));
+    let texto = await fs.promises.readFile(caminho, "utf-8");
+    console.log(extraiLinks(texto))
   } catch (err) {
     trataErro(err);
   }
 }
+
+pegaArquivo("./arquivos/texto1.md");
+
+
+
+
+
+
+
+
+
+
+
+
 
 // function pegaArquivo(caminho) {
 //   fs.promises
@@ -40,6 +71,5 @@ async function pegaArquivo(caminho) {
 //   });
 // }
 
-pegaArquivo("./arquivos/texto1.md");
 
 //console.log(chalk.green(pegaArquivo('./arquivos/texto1.md')))
