7bff94d847e55b8a2cfdaf70a880c46bf1b3064f
henrique <henrique.florencio307@gmail.com>
2022-04-11 17:48:40 -0300

    continuando o curso

--- index.js
+++ index.js
@@ -1,73 +1,9 @@
-//const chalk = require('chalk');
+import { pegaArquivo } from "./cli.js";
 import chalk from "chalk";
-import fs from "fs";
 
-// REGEX https://regex101.com
-// DEVOLVE QUAL QUER QUE TENHA SIDO O ERRO CASO NÃO DOCUMENTADO NA LISTA
-
-function extraiLinks(texto){
-    let regex = /\[([^\]]*)\]\((http?s:\/\/[^$#\s]*.[^\s]*)\)/gm;
-    let arrayResultados = []
-    let temp;
-
-    while((temp = regex.exec(texto)) !== null){
-        arrayResultados.push({ [temp[1]]: temp[2] })
-    }
-
-    return arrayResultados
+async function processaTexto() {
+  const retorno = await pegaArquivo(process.argv[2])
+  console.log(chalk.yellow('lista de links => '), retorno)
 }
 
-extraiLinks(alo)
-
-function trataErro(err) {
-  switch (err.code) {
-    case "ENOENT":
-      throw new Error(chalk.red(err.code, "Caminho especificado ta errado"));
-    default:
-      throw new Error(chalk.red(err.code, "erro não especificado"));
-  }
-}
-
-async function pegaArquivo(caminho) {
-  try {
-    let texto = await fs.promises.readFile(caminho, "utf-8");
-    console.log(extraiLinks(texto))
-  } catch (err) {
-    trataErro(err);
-  }
-}
-
-pegaArquivo("./arquivos/texto1.md");
-
-
-
-
-
-
-
-
-
-
-
-
-
-// function pegaArquivo(caminho) {
-//   fs.promises
-//     .readFile(caminho, "utf-8")
-//     .then((texto) => console.log(texto))
-//     .catch((err) => trataErro(err));
-// }
-
-// function pegaArquivo(caminho) {
-
-//   fs.readFile(caminho, "utf-8", (err, texto) => {
-//     if (err) {
-//       throw new Error(chalk.grey(err.code, "Caminho especificado ta errado"));
-//     } else {
-//       console.log(chalk.green(texto));
-//     }
-//   });
-// }
-
-
-//console.log(chalk.green(pegaArquivo('./arquivos/texto1.md')))
+processaTexto()
