64a04370d838d84209aa21b32186be86315fd92d
henrique <henrique.florencio307@gmail.com>
2022-04-13 18:18:25 -0300

    continuando curso

--- http-validation.js
+++ http-validation.js
@@ -1,9 +1,45 @@
-function arrayString(arrayObjetos){
-    return arrayObjetos.map(objeto => Object.values(objeto).join())
+import fetch from 'node-fetch'
+
+function handleError(err) {
+    throw new Error(err)
 }
 
-function validaURLs(arrayURLs) {
-    return arrayString(arrayURLs)
+async function checaStatus(arrayDeURLs){
+    try {
+        const arrayStatus = await Promise
+            .all(arrayDeURLs
+                .map(async url => {
+                    const statusRes = fetch(url)
+                    return (await statusRes).status
+                })
+            )
+        
+        return arrayStatus
+    } catch(err) {
+        handleError(err)
+    }
+}
+
+function arrayString(arrayObjetos){
+    return arrayObjetos
+        .map(objeto => Object
+            .values(objeto).join()
+        )
+}
+
+async function validaURLs(arrayURLs) {
+
+    const links = arrayString(arrayURLs)
+    const statusLinks = await checaStatus(links)
+
+    const resultados = arrayURLs.map((objeto,indice) => (
+        {
+            ...objeto,
+            status: statusLinks[indice]
+        }
+    ))
+
+    return resultados;
 }
 
 export { validaURLs }
